010cfe4363262ac6901241f6dd7ae69e5a5fa512
Ryan Lau <ryanlau92@gmail.com>
2016-02-22 14:05:09 -0600

    refactored solution

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -5,16 +5,28 @@ public class Diamond {
     }
 
     public static void drawFullDiamond(int upperRows){
-        for (int i = 1; i < (upperRows + 4); i += 2){
-            for (int k = 0; k < (4 - (i / 2)); k++){
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+        drawUpperDiamond(upperRows);
+        drawLowerDiamond(upperRows);
+    }
+
+    public static void drawUpperDiamond(int numOfRows){
+        for (int i = 1; i < (numOfRows + 3); i += 2){
+            drawOutContent(i);
         }
-        for (int i = (upperRows + 1); i > 0; i -= 2){
+    }
+
+    public static void drawOutContent(int num){
+        for (int k = 0; k < (4 - (num / 2)); k++){
+            System.out.print(" ");
+        }
+        for (int j = 0; j < num; j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+
+    }
+    public static void drawLowerDiamond(int numOfRows){
+        for (int i = (numOfRows + 1); i > 0; i -= 2){
             for (int k = 0; k < (5 - (i / 2)); k++){
                 System.out.print(" ");
             }
@@ -25,31 +37,19 @@ public class Diamond {
         }
     }
 
-
     private static void drawDiamondWithName(int rowForName) {
         for (int i = 1; i < (rowForName + 1); i += 2){
-            for (int k = 0; k < (4 - (i / 2)); k++){
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+            drawOutContent(i);
         }
 
-        for (int i = 0; i < ( 4 - (rowForName/2)); i++){
+        printMyName(rowForName);
+        drawLowerDiamond(rowForName);
+    }
+
+    public static void printMyName(int spacing){
+        for (int i = 0; i < ( 4 - (spacing/2)); i++){
             System.out.print(" ");
         }
         System.out.println("Ryan");
-
-        for (int i = (rowForName + 1); i > 0; i -= 2){
-            for (int k = 0; k < (5 - (i / 2)); k++){
-                System.out.print(" ");
-            }
-            for (int j = 1; j < i; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
     }
 }
