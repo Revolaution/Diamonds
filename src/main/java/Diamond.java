1624376457369a3bbd72c7c4b17504d649e0563a
Ryan Lau <ryanlau92@gmail.com>
2016-02-22 13:35:50 -0600

    Changed to one class for Diamond

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -1,7 +1,55 @@
 public class Diamond {
     public static void main(String[] args) {
-        DiamondConstructor dC = new DiamondConstructor();
-//        dC.fullDiamond(5);
-        dC.diamondWithName(5);
+        drawFullDiamond(5);
+        drawDiamondWithName(5);
+    }
+
+    public static void drawFullDiamond(int upperRows){
+        for (int i = 1; i < (upperRows + 4); i += 2){
+            for (int k = 0; k < (4 - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = (upperRows + 1); i > 0; i -= 2){
+            for (int k = 0; k < (5 - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+
+    private static void drawDiamondWithName(int rowForName) {
+        for (int i = 1; i < (rowForName + 1); i += 2){
+            for (int k = 0; k < (4 - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 0; i < ( 4 - (rowForName/2)); i++){
+            System.out.print(" ");
+        }
+        System.out.println("Ryan");
+
+        for (int i = (rowForName + 1); i > 0; i -= 2){
+            for (int k = 0; k < (5 - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
